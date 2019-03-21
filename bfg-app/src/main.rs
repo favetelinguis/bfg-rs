01f6eeec488fde0b3b08aacb00d777812302ed1b
Henrik Larsson <reg@henkelarsson.se>
2019-03-21 20:35:35 +0100

    Started tui impl

--- bfg-app/src/main.rs
+++ bfg-app/src/main.rs
@@ -1,5 +1,38 @@
-use bfg_core;
+//!
+//! This crate covers information about how things is.
+//!
+use log::{debug, error, info, trace, warn};
+use std::io::{stdin, stdout, Error, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+
 fn main() {
-    let num = 10;
-    println!("Hello, world! {}", bfg_core::add_one(num));
+    stderrlog::new().quiet(false).verbosity(4).init().unwrap(); // Init logger impl
+    let stdout = stdout().into_raw_mode().unwrap();
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    // TODO check events.rs and implement my own event handler for tick events and input events
+    // TODO make more stuff from the termion_demo.rs
+
+    trace!("trace message");
+    debug!("debug message");
+    info!("info message");
+    warn!("warn message");
+    error!("error message");
+
+    terminal.draw(|mut f| {
+        let size = f.size();
+        Block::default()
+            .title("Block")
+            .borders(Borders::ALL)
+            .render(&mut f, size);
+    });
 }
--- bfg-core/src/lib.rs
+++ bfg-core/src/lib.rs
@@ -1,5 +1,33 @@
+//!
+//! Holds the core logic and traits for bfg
+//!
+//!
+
+///# Examples
+/// ```
+/// let five = 5;
+/// assert_eq!(6, bfg_core::add_one(5));
+/// ```
 pub fn add_one(x: i32) -> i32 {
     x + 1
+}
+
+/// Main structure for holding a Bfg
+#[derive(Default, Debug, PartialEq)]
+pub struct Bfg {
+    pub one: i32,
+    pub two: i32,
+}
+
+impl Bfg {
+    /// # Examples
+    /// ```
+    /// let bfg = bfg_core::Bfg {one: 1, two: 2};
+    /// assert_eq!(bfg, bfg_core::Bfg::new());
+    /// ```
+    pub fn new() -> Bfg {
+        Bfg { one: 1, two: 2 }
+    }
 }
 
 #[cfg(test)]
